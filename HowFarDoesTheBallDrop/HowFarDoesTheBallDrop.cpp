3f790fc31209d13e0039899bfec1e13fbe3ff2c5
= <seanparsons2003@gmail.com>
2025-08-24 19:05:30 +0100

    removed cmath import, added function to see how long ball will drop at a certain second.

--- HowFarDoesTheBallDrop/HowFarDoesTheBallDrop.cpp
+++ HowFarDoesTheBallDrop/HowFarDoesTheBallDrop.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include<cmath>
 //this program will model how far a ball drops in X seconds from a height of Y meters
 //gravity is 9.8m/s^2
 
+double getDistanceFallen(double seconds) 
+{
+    const double GRAVITY = 9.8;
+    double distanceFallen{(GRAVITY * (seconds*seconds))/2.0};
+    return distanceFallen;
+    
+}
 
 int main()
 {
@@ -11,8 +17,13 @@ int main()
     std::cout << "Input the height of the tower in meters\n";
     std::cin >> height;
 
-    int seconds{};
+    double seconds{};
     std::cout << "Input the seconds it falls for\n";
     std::cin >> seconds;
+
+    std::cout << "At " << seconds << " seconds the ball is at " <<
+        height - getDistanceFallen(seconds) << " meters";
+
+    return 0;
 }
 
