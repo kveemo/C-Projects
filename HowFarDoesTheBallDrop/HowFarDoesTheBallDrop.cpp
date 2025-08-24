37d2236f4e1c7c0b763ad093a6163efd2ad8e1c2
= <seanparsons2003@gmail.com>
2025-08-24 18:50:37 +0100

    New project where user can input a height and time to see how far a ball drops in X seconds

---
+++ HowFarDoesTheBallDrop/HowFarDoesTheBallDrop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cmath>
+//this program will model how far a ball drops in X seconds from a height of Y meters
+//gravity is 9.8m/s^2
+
+
+int main()
+{
+
+    double height{};
+    std::cout << "Input the height of the tower in meters\n";
+    std::cin >> height;
+
+    int seconds{};
+    std::cout << "Input the seconds it falls for\n";
+    std::cin >> seconds;
+}
+
