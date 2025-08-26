7af770243f3a4833ff57571a163855921349894d
= <seanparsons2003@gmail.com>
2025-08-26 20:22:27 +0100

    moved getting user input to a function to condense code

--- HowFarDoesTheBallDrop/HowFarDoesTheBallDrop.cpp
+++ HowFarDoesTheBallDrop/HowFarDoesTheBallDrop.cpp
@@ -2,6 +2,23 @@
 //this program will model how far a ball drops in X seconds from a height of Y meters
 //gravity is 9.8m/s^2
 
+double getInput()
+{
+    double input{};
+    bool isValid{ false };
+
+    while (!isValid) {
+        
+        std::cin >> input;
+        if (input <= 0)
+            std::cout << "Cant be negative or 0!! \n";
+        else
+            isValid = true;
+    }
+
+    return input;
+}
+
 double getDistanceFallen(double seconds) 
 {
     const double GRAVITY = 9.8;
@@ -14,30 +31,16 @@ int main()
 {
 
     double height{};
-    bool isValidHeight{ false };
 
-    while (!isValidHeight) {
-        std::cout << "Input the height of the tower in meters\n";
-        std::cin >> height;
-        if (height <= 0)
-            std::cout << "Cant have a negative or 0 height!! \n";
-        else
-            isValidHeight = true;
-    }
+    std::cout << "Input the height of the tower in meters\n";
+    height = getInput();
 
     double seconds{};
     bool isValidTime{ false };
     
-    while (!isValidTime) {
-        std::cout << "Input the seconds it falls for\n";
-        std::cin >> seconds;
-        if (seconds < 0)
-            std::cout << "Cant fall for negative seconds!! \n";
-        else
-            isValidTime = true;
-    }
+    std::cout << "Input the seconds it falls for\n";
+    seconds = getInput();
     
-
     int i{ 0 };
     while (i <= seconds) 
     {
